5e5294aac73f3d7f9e61cdcfe3d89a287b848d6d
Justin Lewis Salmon <justin2.salmon@live.uwe.ac.uk>
2015-06-20 11:55:05 +0200

    Initial source commit

---
+++ src/main/java/org/cosmicpi/daq/Application.java
@@ -0,0 +1,15 @@
+package org.cosmicpi.daq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jsalmon on 19/06/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
